10e8e14bfa925b206cb29c926b4e80f67ce4df63
luong228 <90311663+luong228@users.noreply.github.com>
2023-06-12 22:26:39 +0700

    Add UI for menu, eraser tool

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById("canvas-area");
 var context = canvas.getContext("2d");
-var isDrawing = false;
+var isDrawing = true;
+var isErasing = false;
+var isMouseDown = false;
+var color = "black";
 
-canvas.addEventListener("mousedown", function(event) {
+document.getElementById("pencil").addEventListener("click", () => {
     isDrawing = true;
-    context.beginPath();
-    context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    isErasing = false;
+})
+
+document.getElementById("eraser").addEventListener("click", () => {
+    isDrawing = false;
+    isErasing = true;
+})
+
+document.getElementById("color").addEventListener("click", () => {
+    color = prompt("Enter a color:");
+})
+
+canvas.addEventListener("mousedown", (event) => {
+    if (isDrawing) {
+        context.beginPath();
+        context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    }
+    if (isErasing) {
+        context.clearRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 10, 10)
+    }
+    isMouseDown = true;
 });
 
-canvas.addEventListener("mousemove", function(event) {
-    if (isDrawing) {
+canvas.addEventListener("mousemove", (event) => {
+    if (isDrawing && isMouseDown) {
         context.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        context.strokeStyle = color;
         context.stroke();
+    }
+    if (isErasing && isMouseDown ) {
+        context.clearRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 10, 10)
     }
 });
 
-canvas.addEventListener("mouseup", function(event) {
-    isDrawing = false;
+canvas.addEventListener("mouseup", (event) => {
+    // isDrawing = false;
+    // isErasing = false;
+    isMouseDown = false;
 });
 
-function save() {
+// Menu 
+document.getElementById("save").addEventListener("click", () => {
     var link = document.createElement('a');
     link.download = 'drawing.png';
     link.href = canvas.toDataURL();
     link.click();
-}
+});
+
+document.getElementById("undo").addEventListener("click", function() {
+    // undo
+});
+
+document.getElementById("redo").addEventListener("click", function() {
+    // redo
+});
