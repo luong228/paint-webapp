5533bc3b5e9d19039e5ad9a3299c57b5012c7b87
luong228 <90311663+luong228@users.noreply.github.com>
2023-06-14 21:54:13 +0700

    Complete File menu + Effect  Pencil and Eraser

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
-var canvas = document.getElementById("canvas-area");
-var context = canvas.getContext("2d");
-var isDrawing = true;
-var isErasing = false;
-var isMouseDown = false;
-var color = "black";
+const canvas = document.getElementById("canvas-area");
+const context = canvas.getContext("2d");
+let isDrawing = true;
+let isErasing = false;
+let isMouseDown = false;
+let color = "black";
 
+
+const openImage = document.getElementById('openImage');
+const imageLoader = document.getElementById('imageLoader');
+openImage.addEventListener('click', function () {
+    imageLoader.click(); // Kích hoạt phần tử <input type="file"> ẩn
+});
+
+imageLoader.addEventListener('change', handleImage, false);
+
+function handleImage(e) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+}
+let pencilBtn = document.querySelector("#pencil")
+pencilBtn.classList.add("btn-focus_focus")
+let btnsFocus = document.querySelectorAll('.btn-focus');
+
+btnsFocus.forEach(function (button) {
+    button.addEventListener("mousedown", function () {
+        button.classList.add("btn-focus_focus")
+        btnsFocus.forEach(function (otherButton) {
+            if (otherButton != button) {
+                otherButton.classList.remove("btn-focus_focus");
+            }
+        })
+    });
+
+});
 document.getElementById("pencil").addEventListener("click", () => {
     isDrawing = true;
     isErasing = false;
+    document.body.insertAdjacentHTML('beforeend',
+        "<style>body{cursor: url('pencil.svg'), auto;} </style>");
+    this.focus();
 })
-
 document.getElementById("eraser").addEventListener("click", () => {
     isDrawing = false;
     isErasing = true;
+    document.body.insertAdjacentHTML('beforeend',
+        "<style>body{cursor: url('square.svg'), auto;} </style>");
+    this.focus();
 })
 
-document.getElementById("color").addEventListener("click", () => {
-    color = prompt("Enter a color:");
-})
+// document.getElementById("color").addEventListener("click", () => {
+//     color = prompt("Enter a color:");
+// })
 
 canvas.addEventListener("mousedown", (event) => {
     if (isDrawing) {
@@ -25,7 +67,7 @@ canvas.addEventListener("mousedown", (event) => {
         context.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
     }
     if (isErasing) {
-        context.clearRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 10, 10)
+        context.clearRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 15, 15)
     }
     isMouseDown = true;
 });
@@ -36,8 +78,8 @@ canvas.addEventListener("mousemove", (event) => {
         context.strokeStyle = color;
         context.stroke();
     }
-    if (isErasing && isMouseDown ) {
-        context.clearRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 10, 10)
+    if (isErasing && isMouseDown) {
+        context.clearRect(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop, 15, 15)
     }
 });
 
@@ -48,17 +90,183 @@ canvas.addEventListener("mouseup", (event) => {
 });
 
 // Menu 
-document.getElementById("save").addEventListener("click", () => {
-    var link = document.createElement('a');
-    link.download = 'drawing.png';
-    link.href = canvas.toDataURL();
-    link.click();
-});
+const saveButtons = document.querySelectorAll(".save");
 
-document.getElementById("undo").addEventListener("click", function() {
-    // undo
-});
+saveButtons.forEach((item) => {
+    item.addEventListener("click", () => {
+        const link = document.createElement('a');
+        link.download = 'drawing.png';
+        link.href = canvas.toDataURL();
+        link.click();
+    }
+    )
+})
 
-document.getElementById("redo").addEventListener("click", function() {
-    // redo
+const saveAsPNG = document.querySelector("#saveAsPNG");
+saveAsPNG.addEventListener("click", function () {
+
+    const image = canvas.toDataURL("image/png");
+
+    const blob = new Blob([image], { type: "image/png" });
+
+    const fileName = prompt("Enter a file name", "myImage.png");
+
+    if (fileName != null) {
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.style.display = "none";
+        document.body.appendChild(link);
+
+        link.click();
+
+        setTimeout(function () {
+            URL.revokeObjectURL(link.href);
+            document.body.removeChild(link);
+        }, 0);
+    }
+});
+const saveAsBMP = document.querySelector("#saveAsBMP");
+saveAsBMP.addEventListener("click", function () {
+
+    const image = canvas.toDataURL("image/bmp");
+
+    const blob = new Blob([image], { type: "image/bmp" });
+
+    const fileName = prompt("Enter a file name", "myImage.bmp");
+
+    if (fileName != null) {
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.style.display = "none";
+        document.body.appendChild(link);
+
+        link.click();
+
+        setTimeout(function () {
+            URL.revokeObjectURL(link.href);
+            document.body.removeChild(link);
+        }, 0);
+    }
+});
+const saveAsJPEG = document.querySelector("#saveAsJPEG");
+saveAsJPEG.addEventListener("click", function () {
+
+    const image = canvas.toDataURL("image/jpeg");
+
+    const blob = new Blob([image], { type: "image/jpeg" });
+
+    const fileName = prompt("Enter a file name", "myImage.jpeg");
+
+    if (fileName != null) {
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.style.display = "none";
+        document.body.appendChild(link);
+
+        link.click();
+
+        setTimeout(function () {
+            URL.revokeObjectURL(link.href);
+            document.body.removeChild(link);
+        }, 0);
+    }
+});
+const saveAsGIF = document.querySelector("#saveAsGIF");
+saveAsGIF.addEventListener("click", function () {
+
+    const image = canvas.toDataURL("image/gif");
+
+    const blob = new Blob([image], { type: "image/gif" });
+
+    const fileName = prompt("Enter a file name", "myImage.gif");
+
+    if (fileName != null) {
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.style.display = "none";
+        document.body.appendChild(link);
+
+        link.click();
+
+        setTimeout(function () {
+            URL.revokeObjectURL(link.href);
+            document.body.removeChild(link);
+        }, 0);
+    }
+});
+const saveAsOther = document.querySelector("#saveAsOther");
+saveAsOther.addEventListener("click", function () {
+
+    const image = canvas.toDataURL("image/png");
+
+    const blob = new Blob([image], { type: "image/png" });
+
+    const fileName = prompt("Enter a file name", "myImage.png");
+
+    if (fileName != null) {
+
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = fileName;
+        link.style.display = "none";
+        document.body.appendChild(link);
+
+        link.click();
+
+        setTimeout(function () {
+            URL.revokeObjectURL(link.href);
+            document.body.removeChild(link);
+        }, 0);
+    }
+});
+const newBtn = document.querySelector("#newCanvas")
+newBtn.addEventListener("click", () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+})
+function printCanvas() {
+    const dataUrl = document.getElementById("canvas-area").toDataURL();
+    let windowContent = '<!DOCTYPE html><html><head><title>Print</title></head><body><img src="' + dataUrl + '"></body></html>';
+    const printWin = window.open("", "", "width=" + screen.availWidth + ",height=" + screen.availHeight);
+    printWin.document.open();
+    printWin.document.write(windowContent);
+    printWin.document.addEventListener("load", function () {
+        printWin.focus();
+        printWin.print();
+        setTimeout(function () {
+            printWin.document.close();
+            printWin.close();
+        }, 901);
+    }, true);
+}
+const printBtn = document.querySelector("#printBtn");
+printBtn.addEventListener("click", printCanvas)
+//   document.getElementsByClassName("undo").addEventListener("click", function() {
+//     // undo
+// });
+
+// document.getElementsByClassName("redo").addEventListener("click", function() {
+//     // redo
+// });
+$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = $(this).next('.dropdown-menu');
+    $subMenu.toggleClass('show');
+
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+        $('.dropdown-submenu .show').removeClass('show');
+    });
+
+
+    return false;
 });
